6f30a42de0d5c2dc8486e7824576b0c7d7c982a4
Prabhav Sohgaura <prabhavsohgaura333@gmail.com>
2021-02-25 18:49:25 +0530

    updated commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+  include_once("index.html");
+  include_once("about.html");
+  include_once("service.html");
+  include_once("contact.html");
+  include_once("style.css");
+  include_once("main.js");
+  include_once("images");
+  
+?>
